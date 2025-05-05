53a3df1be3177b27b87b1ab7efcfea7d1207ce7a
Lyvdev109 <olyvia.lindner@bellevuecollege.edu>
2025-05-05 11:00:07 -0700

    Update example.js
    multiplication tab;e

--- m05/js/example.js
+++ m05/js/example.js
@@ -4,7 +4,7 @@ var operator = 'addition'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
+/*if (operator === 'addition') {
   // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
@@ -16,6 +16,21 @@ if (operator === 'addition') {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
+}*/
+
+// table is for multiplicatoin 
+if (operator === 'multiplication') {
+  //do multiplication
+while (i<11) {
+  msg += + 'x' + table + '=' + (i * table) + '<br />';
+  i++;
+  }
+} else {
+  // do addition
+while (i < 11) {
+  msg += i + '+' + table + '=' + (i + table) + '<br />';
+  i++;
+}
 }
 
 // Write the message into the page
