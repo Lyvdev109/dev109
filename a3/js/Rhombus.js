4e21baf45a357f76f294464dc7999844e0c839f0
Lyvdev109 <olyvia.lindner@bellevuecollege.edu>
2025-05-20 22:41:58 -0700

    Update Rhombus.js
    bottom left

--- a3/js/Rhombus.js
+++ a3/js/Rhombus.js
@@ -10,6 +10,8 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol)
 {
   upRight(pHeight, pColorEven, pColorOdd, pSymbol);
   downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
 // Top right part of the Rhombus
@@ -63,7 +65,8 @@ function downRight(pHeight, pColorEven, pColorOdd, pSymbol)
   document.getElementById("downRight").innerHTML = rLine;
 }
 
-function upLeft(pHeight, pColorEven, pColoroff, pSymbol)
+// Upper left portion of the Rhombus
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol)
 {
   var rLine ="";
   for (i=pHeight; i > 0 ; i--) {
@@ -85,3 +88,29 @@ function upLeft(pHeight, pColorEven, pColoroff, pSymbol)
   }
   document.getElementById("upLeft").innerHTML = rLine;
 }
+
+// Bottom left prortion of Rhombus
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol)
+{
+  var rLine = "";
+  for (i=pHeight; i>0 ; i--) {
+    rLine += "<p>";
+
+//Create each line of Rhombus
+    for (j=0; j<i; j++) {
+
+// Is the position EVEN or ODD so we change the color
+      if (j%2)
+//EVEN
+        rLine += "<span style = 'color:" + pColorEven + ";'>" + pSymbol +" </span>";
+      else
+// ODD
+        rLine += "<span style = 'color:" + pColorOdd + ";'>" + pSymbol +" </span>";
+    }
+    rLine += "<p>";
+//console.log(rLine);
+  }
+  document.getElementById("downLeft").innerHTML = rLine;
+}
+
+
