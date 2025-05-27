512409a23a188ccc893f3eba20a4db030d181a67
Lyvdev109 <olyvia.lindner@bellevuecollege.edu>
2025-05-27 11:03:34 -0700

    Update hotel.js

--- m08/js/hotel.js
+++ m08/js/hotel.js
@@ -1,1 +1,20 @@
+//Create the template for objects that are hotels
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function() {
+    return this.rooms - this.booked;
+  };
+}
+
+//Create two hotel objects
+var quayHotel = new Hotel('Quay', 40, 25);
+var parkHotel = new Hotel('Park', 120, 77);
+
+//Update the HTML for the page
+var detail1 = quayHotel.name + 'rooms: ';
+  details1 += quayHotel.checkAvailability();
+var elHotel2 = documant.getElementById('hotel2');
+elHotel2.textContent = details2;
 
