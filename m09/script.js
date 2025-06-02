b317a0c94183deaa4d28015d5b67ab3129b0c30f
Lyvdev109 <olyvia.lindner@bellevuecollege.edu>
2025-06-02 15:32:13 -0700

    Update script.js

--- m09/script.js
+++ m09/script.js
@@ -12,3 +12,35 @@ var position = document.getElementsByTagName('ul')[0];
 
 // Insert the new element into its position.
 position.appendChild(newEl);
+
+// adding button
+var addItemInput = document.getElementById('newItem');
+var addItemButton = document.getElementById('addItemButton');
+var groceryList = document.getElementById('todo')
+
+// function to the buttons onClick property
+addItemButton.onclick = function() {
+  var newItemText = addItemInput.value.trim ();
+
+// check to see if input is empty
+  if (newItemText !== "") {
+// create new list item
+    var newListItem = document.createElement('li');
+
+// create a text node with the user input
+  var newListItemText = document.createTextNode(newItemText);
+
+// append the text to a new line
+  newListItem.appendChild(newListItemText);
+
+// append the new list to existing unordered list
+  groceryList.appendChild(newListItem);
+
+// clear input after adding item
+  addItem.value = '';
+  addItemInput.focus();
+}
+else {
+  alert("please enter an item to add to the list.");
+}
+};
